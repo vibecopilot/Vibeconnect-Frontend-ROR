235549a2efa91f1ab1988b1175426c66d56300eb
mittu-panda <mittupanda7@gmail.com>
2024-12-14 14:54:24 +0530

    fnb live update

--- src/pages/Setup/ParkingSetupPages/ParkingTag.jsx
+++ src/pages/Setup/ParkingSetupPages/ParkingTag.jsx
@@ -6,8 +6,16 @@ import { useSelector } from "react-redux";
 import { BiEdit } from "react-icons/bi";
 
 import ToggleSwitch from "../../../Buttons/ToggleSwitch";
+import { FaTimes } from "react-icons/fa";
 //import Modal from "../containers/modals/Modal";
 const ParkingTag = () => {
+  const [selectedColor, setSelectedColor] = useState("#eb5768");
+  const handleInputChange = (e) => {
+    setSelectedColor(e.target.value);
+  };
+  const handleChange = (e) => {
+    setColor(e.target.value); // Update color state
+  };
     const themeColor = useSelector((state)=> state.theme.color)
     const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -25,55 +33,76 @@ const ParkingTag = () => {
            {isModalOpen && (
         <div className="fixed inset-0 flex items-center justify-center z-50">
           <div className="fixed inset-0 bg-black bg-opacity-50" onClick={closeModal}></div>
-          <div className="bg-white w-[400px] h-[400px] rounded-lg shadow-lg p-4 relative z-10">
+          <div className="bg-white w-96 rounded-lg shadow-lg p-4 relative z-10">
             <button
-              className="absolute top-2 right-2 text-gray-600 hover:text-gray-900"
+              className="absolute top-4 right-2 text-gray-600 hover:text-gray-900"
               onClick={closeModal}
             >
-              &times;
+             <FaTimes size={20}/>
             </button>
-            <h2 className="text-xl font-semibold mb-4">Edit Tag</h2>
-            <form>
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="category-name">
+            <h2 className="text-xl text-black font-semibold mb-4">Edit Tag</h2>
+            <div>
+              <div className="flex flex-col gap-1 mb-2">
+                <label className="block text-gray-700  " htmlFor="category-name">
                 Company Tag Name*
                 </label>
                 <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  className="border p-1 px-4 border-gray-500 rounded-md"
                   id="category-name"
                   type="text"
-                  placeholder="Category Name"
+                  placeholder="Company Tag Name"
                 />
               </div>
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="subcategory">
+              <div className="flex flex-col gap-1 mb-2">
+                <label                  
+ htmlFor="subcategory">
                 Tag Type*
                 </label>
                 <select
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  className="border p-1 px-4 border-gray-500 rounded-md"
                   id="subcategory"
                   type="text"
                   placeholder="SubCategory"
-                />
+                >
+                  <option value="">Select Tag Type</option>
+                  <option value="product">Product</option>
+                  <option value="client">Client</option>
+                </select>
               </div>
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="description">
-                Tag Colour*
-                </label>
-                <select
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="description"
-                  type="text"
-                  placeholder="Description"
-                />
-              </div>
-              <div>
+              <div className="flex flex-col gap-1">
+      <label htmlFor="color-picker" className="font-semibold">
+        Tag Color
+      </label>
+      <div className="flex ">
+      {/* <label className="block text-gray-700 text-sm font-bold">Pick a Color:</label> */}
+    
+      <input
+        type="text"
+        value={selectedColor}
+        name="color" 
+        id="color"        
+        onChange={handleInputChange}
+        className="border p-2 text-center border border-gray-700 rounded-md w-64"
+        placeholder="Enter Hex Code"
+      />
+        <input
+        type="color"
+        value={selectedColor}
+        onChange={handleInputChange}
+        className="w-5 h-5 cursor-pointer rounded-full mt-2 ml-2"
+        style={{ backgroundColor: selectedColor }}
+      />
+    </div>
+    </div>
+              <div className="my-4">
                 <input type="checkbox" className="ml-2"/>
-                <label htmlFor="" className="font-semibold ml-2"><b>MOM</b></label>
+                <label htmlFor="" className="font-semibold ml-2">MOM</label>
                 <input type="checkbox" className="ml-4"/>
-                <label htmlFor="" className="font-semibold ml-2"><b>Task</b></label>
+                <label htmlFor="" className="font-semibold ml-2">Task</label>
             </div>
-            <div><br /></div>
+            <div>
+
+            </div>
               <div className="flex items-center justify-between">
                 <button
                   className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
@@ -82,15 +111,15 @@ const ParkingTag = () => {
                 >
                   Update
                 </button>
-                <button
+                {/* <button
                   className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                   type="button"
                   onClick={closeModal}
                 >
                   Cancel
-                </button>
+                </button> */}
               </div>
-            </form>
+            </div>
           </div>
         </div>
       )}
@@ -100,7 +129,14 @@ const ParkingTag = () => {
     },
     { name: "ID", selector: (row) => row.id, sortable: true },
 
-    { name: "Active/Inactive", selector: (row) => row.active, sortable: true },
+    {
+      name: "Active/Inactive",
+      selector: (row) => (
+        <input type="checkbox" checked={row.isActive} onChange={() => handleCheckboxChange(row)} />
+      ),
+      sortable: true,
+    },
+    
     { name: "Company Tag Name", selector: (row) => row.tname, sortable: true },
     { name: "Tag Type", selector: (row) => row.tag_type, sortable: true },
     { name: "MOM", selector: (row) => row.MOM, sortable: true },
@@ -111,7 +147,7 @@ const ParkingTag = () => {
   const data = [
     {
       id: 1,
-      active:<ToggleSwitch/>,
+      
       tname:"Urgent",
       tag_type:"others",
       MOM:"Yes",
@@ -138,13 +174,13 @@ const ParkingTag = () => {
       {/* <Navbar /> */}
       <div className="w-full flex mx-3 flex-col overflow-hidden">
 
-        <div className=" flex m-3 flex-row">
+        <div className="flex gap-2 my-2 ">
           <div className="flex gap-1 items-center flex-col">
-            <label htmlFor="" className="font-semibold">Company Tag Name</label>
+            <label htmlFor="" className="font-semibold">Company Tag Name*</label>
             <input
               type="text"
-              placeholder="Enter Tag Name"
-              className="border-2 p-2  border-gray-300 rounded-lg"
+              placeholder="Enter Company Tag Name"
+              className="border p-2 px-4 border-gray-300 rounded-md"
             />
             </div>
             <div className="flex gap-1 items-center flex-col">
@@ -152,21 +188,44 @@ const ParkingTag = () => {
             <select
               type="text"
               placeholder="Enter Tag Type"
-              className="border-2 p-2 w-48 ml-2 border-gray-300 rounded-lg"
-            />
+              className="border p-2 px-4 w-64 border-gray-300 rounded-md"
+              >
+                <option value="">Select Tag Type</option>
+                <option value="product">Product</option>
+                <option value="client">Client</option>
+              </select>
+            
             </div>
-            <div className="ml-6">
-         <label htmlFor="" className="font-semibold">Tag Color</label>
-         <select name="" id="" className="border border-gray-400 p-2 rounded-md w-full">
-          <option value="">Category1</option>
-          <option value="">Category2</option>
-         </select>
-         </div>
-            <div>
+            <div className="flex flex-col gap-1">
+      <label htmlFor="color-picker" className="font-semibold">
+        Tag Color
+      </label>
+      <div className="flex ">
+      {/* <label className="block text-gray-700 text-sm font-bold">Pick a Color:</label> */}
+    
+      <input
+        type="text"
+        value={selectedColor}
+        name="color" 
+        id="color"        
+        onChange={handleInputChange}
+        className="border p-2 text-center border border-gray-700 rounded-md w-64"
+        placeholder="Enter Hex Code"
+      />
+        <input
+        type="color"
+        value={selectedColor}
+        onChange={handleInputChange}
+        className="w-5 h-5 cursor-pointer rounded-full mt-2 ml-2"
+        style={{ backgroundColor: selectedColor }}
+      />
+    </div>
+    </div>
+            <div className="flex items-center">
                 <input type="checkbox" className="ml-2"/>
                 <label htmlFor="" className="font-semibold ml-2">MOM</label>
             </div>
-            <div>
+            <div className="flex items-center">
                 <input type="checkbox" className="ml-4"/>
                 <label htmlFor="" className="font-semibold ml-2">Task</label>
             </div>
