a0cc9725af9cf7690ceac74fcff95af5800873bb
amanraturi <amaninbox59@gmail.com>
2025-02-12 13:55:37 +0530

    parking

--- src/pages/SubPages/AddParking.jsx
+++ src/pages/SubPages/AddParking.jsx
@@ -1,103 +1,105 @@
 import React, { useEffect, useState } from "react";
 import Select from "react-select";
-import { getAssignedTo, getAvailableParkingNumber, getFloors, getParkingConfiguration, getParkingSlots, postParking } from "../../api";
+import {
+  getAssignedTo,
+  getAvailableParkingNumber,
+  getFloors,
+  getParkingConfiguration,
+  getParkingSlots,
+  postParking,
+} from "../../api";
 import { useSelector } from "react-redux";
 import { getItemInLocalStorage } from "../../utils/localStorage";
 import Navbar from "../../components/Navbar";
+import { useNavigate } from "react-router-dom";
 
 const AddParking = () => {
-  const [behalf, setBehalf] = useState("self")
-  const[users, setUsers] = useState([])
+  const navigate  = useNavigate()
+  const [behalf, setBehalf] = useState("self");
+  const [users, setUsers] = useState([]);
   const [floors, setFloors] = useState([]);
-   
-    const siteId = getItemInLocalStorage("SITEID");
-    const userId = getItemInLocalStorage("UserId");
-     const [ParkConfigData, setParkConfigData] = useState([]);
-        useEffect(() => {
-          const fetchPantry = async () => {
-           try {
-             const invResp = await getAvailableParkingNumber();
-             const sortedInvData = invResp.data.data.sort((a, b) => {
-               
-              return new Date(b.created_at) - new Date(a.created_at);
-            });
-             
-            setParkConfigData(sortedInvData)
-             console.log("Available parking number",invResp);
-           } catch (error) {
-            console.log(error)
-           }
-          };
-          fetchPantry();
-        }, []);
-     const [filteredData, setFilteredData] = useState([]);
-          useEffect(() => {
-            const fetchPantry = async () => {
-             try {
-               const invResp = await getParkingSlots();
-               const sortedInvData = invResp.data.sort((a, b) => {
-                 
-                return new Date(b.created_at) - new Date(a.created_at);
-              });
-               
-               setFilteredData(sortedInvData)
-               console.log(invResp);
-             } catch (error) {
-              console.log(error)
-             }
-            };
-            fetchPantry();
-          }, []);
+
+  const siteId = getItemInLocalStorage("SITEID");
+  const userId = getItemInLocalStorage("UserId");
+  const [ParkConfigData, setParkConfigData] = useState([]);
+  useEffect(() => {
+    const fetchPantry = async () => {
+      try {
+        const invResp = await getAvailableParkingNumber();
+        const sortedInvData = invResp.data.data.sort((a, b) => {
+          return new Date(b.created_at) - new Date(a.created_at);
+        });
+
+        setParkConfigData(sortedInvData);
+        console.log("Available parking number", invResp);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchPantry();
+  }, []);
+  const [filteredData, setFilteredData] = useState([]);
+  useEffect(() => {
+    const fetchPantry = async () => {
+      try {
+        const invResp = await getParkingSlots();
+        const sortedInvData = invResp.data.sort((a, b) => {
+          return new Date(b.created_at) - new Date(a.created_at);
+        });
+
+        setFilteredData(sortedInvData);
+        console.log(invResp);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchPantry();
+  }, []);
   const [formData, setFormData] = useState({
-    on_behalf:"",
+    on_behalf: "",
     booking_date: "",
     booking_start_time: "",
     booking_end_time: "",
     slot_id: "",
-    parking_id:"",
-    building_id:"",
-    floor_id:"",
-    user_id:"",
-    status:""
-  })
-  console.log(formData)
-  const buildings = getItemInLocalStorage("Building")
+    parking_id: "",
+    building_id: "",
+    floor_id: "",
+    user_id: "",
+    status: "",
+  });
+  console.log(formData);
+  const buildings = getItemInLocalStorage("Building");
   const handleChange = async (e) => {
-      
-      async function fetchFloor(floorID) {
-        try {
-          const build = await getFloors(floorID);
-          setFloors(build.data.map((item) => ({ name: item.name, id: item.id })));
-        } catch (e) {
-          console.log(e);
-        }
+    async function fetchFloor(floorID) {
+      try {
+        const build = await getFloors(floorID);
+        setFloors(build.data.map((item) => ({ name: item.name, id: item.id })));
+      } catch (e) {
+        console.log(e);
       }
-  
-      
-  
-      if (e.target.type === "select-one" && e.target.name === "building_id") {
-        const BuildID = Number(e.target.value);
-        await fetchFloor(BuildID);
-  
-        setFormData({
-          ...formData,
-          building_id: BuildID,
-        });
-      } 
-      
-       else {
-        setFormData({
-          ...formData,
-          [e.target.name]: e.target.value,
-        });
-      }
-    };
-    const formatTime = (hour, minute) => {
-      const period = hour >= 12 ? "PM" : "AM";
-      const formattedHour = hour % 12 || 12; // Convert 0 to 12 for midnight
-      const formattedMinute = minute?.toString().padStart(2, '0') || "00"; // Ensure two digits for minutes
-      return `${formattedHour}:${formattedMinute} ${period}`;
-    };
+    }
+
+    if (e.target.type === "select-one" && e.target.name === "building_id") {
+      const BuildID = Number(e.target.value);
+      await fetchFloor(BuildID);
+
+      setFormData({
+        ...formData,
+        building_id: BuildID,
+      });
+    } else {
+      setFormData({
+        ...formData,
+        [e.target.name]: e.target.value,
+      });
+    }
+  };
+  const formatTime = (hour, minute) => {
+    const period = hour >= 12 ? "PM" : "AM";
+    const formattedHour = hour % 12 || 12; // Convert 0 to 12 for midnight
+    const formattedMinute = minute?.toString().padStart(2, "0") || "00"; // Ensure two digits for minutes
+    return `${formattedHour}:${formattedMinute} ${period}`;
+  };
   useEffect(() => {
     const fetchAssignedTo = async () => {
       try {
@@ -118,19 +120,23 @@ const AddParking = () => {
   }, []);
   const userIdToSend = behalf === "self" ? userId : formData.user_id;
   const handleSubmit = async () => {
-   
-
     const sendData = new FormData();
     sendData.append("booking_parking[booking_date]", formData.booking_date);
     sendData.append("booking_parking[user_id]", userIdToSend);
-    sendData.append("booking_parking[booking_start_time]", formData.booking_start_time);
-    sendData.append("booking_parking[booking_end_time]", formData.booking_end_time);
+    sendData.append(
+      "booking_parking[booking_start_time]",
+      formData.booking_start_time
+    );
+    sendData.append(
+      "booking_parking[booking_end_time]",
+      formData.booking_end_time
+    );
     sendData.append("booking_parking[slot_id]", formData.slot_id);
     sendData.append("booking_parking[parking_id]", formData.parking_id);
     sendData.append("booking_parking[status]", "Booked");
 
     sendData.append("booking_parking[site_id]", siteId);
-    
+
     try {
       const resp = await postParking(sendData);
       navigate("/admin/parking");
@@ -140,18 +146,24 @@ const AddParking = () => {
       console.log(error);
     }
   };
-  const themeColor = useSelector((state)=> state.theme.color)
+  const themeColor = useSelector((state) => state.theme.color);
   return (
     <section className="flex ">
-<Navbar/>
+      <Navbar />
       <div className="flex flex-col md:items-center mb-10 p-4 w-full">
-        <div style={{background: themeColor}} className="flex justify-center bg-black mx-5 my-2 w-full p-2 rounded-md">
-          <h2 className="text-xl font-semibold text-center mx-10 text-white w-full" style={{background: themeColor}}>
+        <div
+          style={{ background: themeColor }}
+          className="flex justify-center bg-black mx-5 my-2 w-full p-2 rounded-md"
+        >
+          <h2
+            className="text-xl font-semibold text-center mx-10 text-white w-full"
+            style={{ background: themeColor }}
+          >
             Book Parking
           </h2>
         </div>
         <div className="md:border border-gray-400 rounded-md md:mx-10 w-full p-4">
-        <div className="md:grid flex flex-col grid-cols-3 items-center my-2">
+          <div className="md:grid flex flex-col grid-cols-3 items-center my-2">
             <p className="font-semibold">For :</p>
             <div className="flex gap-5">
               <p
@@ -171,10 +183,9 @@ const AddParking = () => {
                 Others
               </p>
             </div>
-           
           </div>
           <div className="flex md:grid  grid-cols-2 justify-between gap-4  flex-col">
-          {behalf === "others" && (
+            {behalf === "others" && (
               // <Select
               //   options={users}
               //   placeholder="Select User"
@@ -186,32 +197,35 @@ const AddParking = () => {
               //   isMulti
               // />
               <div className="flex flex-col">
-              <label className="font-semibold" htmlFor="">Select User</label>
-              <select 
-              name="user_id"
-              value={formData.user_id}
-                onChange={handleChange}
-              className="border p-1 px-4 border-gray-500 rounded-md"
-              >
-                <option value="">Select User</option>
-                {users?.map((building) => (
-                  <option value={building.value} key={building.value}>
-                    {building.label}
-                  </option>
-                ))}
-              </select>
+                <label className="font-semibold" htmlFor="">
+                  Select User
+                </label>
+                <select
+                  name="user_id"
+                  value={formData.user_id}
+                  onChange={handleChange}
+                  className="border p-1 px-4 border-gray-500 rounded-md"
+                >
+                  <option value="">Select User</option>
+                  {users?.map((building) => (
+                    <option value={building.value} key={building.value}>
+                      {building.label}
+                    </option>
+                  ))}
+                </select>
               </div>
             )}
             <div className="flex flex-col">
               {/* <div className="grid grid-cols-2 items-center"> */}
               <p className="font-semibold">Select Date :</p>
-             <input type="date" 
-             name="booking_date" 
-             id="booking_date" 
-             className="border p-1 px-4 border-gray-500 rounded-md"
-             value={formData.booking_date}
+              <input
+                type="date"
+                name="booking_date"
+                id="booking_date"
+                className="border p-1 px-4 border-gray-500 rounded-md"
+                value={formData.booking_date}
                 onChange={handleChange}
-             />
+              />
             </div>
             {/* <div className="flex flex-col ">
               <label htmlFor="" className="font-semibold">
@@ -249,15 +263,15 @@ const AddParking = () => {
               </select>
             </div> */}
             {/* <div className="grid grid-cols-2 items-center "> */}
-            
+
             {/* <div className="flex justify-center mt-5">
               <button className="p-1 px-4 border-2 border-black rounded-md font-semibold hover:bg-black hover:text-white transition-all duration-300">
                 Submit
               </button>
             </div> */}
-          {/* </div> */}
+            {/* </div> */}
 
-          {/* <div className="my-5">
+            {/* <div className="my-5">
             <div className="flex md:grid grid-cols-3 flex-col gap-4 justify-between my-2 md:gap-2"> */}
             {/* <div className="grid  items-center">
                 <label className="font-semibold">Vehicle Type :</label>
@@ -270,7 +284,7 @@ const AddParking = () => {
                   <option value="2 wheeler">2 Wheeler</option>
                 </select>
               </div> */}
-              {/* <div className="flex flex-col">
+            {/* <div className="flex flex-col">
                 <p className="font-semibold ">Employee Name :</p>
                 <select className="border p-1 px-4 border-gray-500 rounded-md">
                   <option value="">Select Employee</option>
@@ -279,7 +293,7 @@ const AddParking = () => {
                   <option value="visitor">Employee C</option>
                 </select>
               </div> */}
-              {/* <div>
+            {/* <div>
                 <div className="grid grid-cols-2 items-center ">
                   <p className="font-semibold ">Free Slot :</p>
                   <p>10</p>
@@ -293,23 +307,25 @@ const AddParking = () => {
                 <p className="font-semibold ">Available Slot :</p>
                 <p>10</p>
               </div> */}
-             
-              <div className="flex flex-col">
+
+            <div className="flex flex-col">
               <p className="font-semibold ">Select Parking Slot :</p>
-              <select className="border p-1 px-4 border-gray-500 rounded-md"
-              name="slot_id"
-              value={formData.slot_id}
-              onChange={handleChange}
+              <select
+                className="border p-1 px-4 border-gray-500 rounded-md"
+                name="slot_id"
+                value={formData.slot_id}
+                onChange={handleChange}
               >
                 <option value="">Select Parking Slot</option>
                 {filteredData?.map((building) => (
-    <option value={building.id} key={building.id}>
-      {formatTime(building.start_hr, building.start_min)} to {formatTime(building.end_hr, building.end_min)}
-    </option>
-  ))}
+                  <option value={building.id} key={building.id}>
+                    {formatTime(building.start_hr, building.start_min)} to{" "}
+                    {formatTime(building.end_hr, building.end_min)}
+                  </option>
+                ))}
               </select>
             </div>
-              {/* <div className="grid  items-center">
+            {/* <div className="grid  items-center">
                 <p className="font-semibold">From :</p>
                 <input
                   type="time"
@@ -331,7 +347,7 @@ const AddParking = () => {
                   className="border p-1 px-4 border-gray-500 rounded-md"
                 />
               </div> */}
-              {/* <div className="flex flex-col">
+            {/* <div className="flex flex-col">
               <p className="font-semibold ">Status :</p>
               <select className="border p-1 px-4 border-gray-500 rounded-md"
               name="status"
@@ -345,28 +361,34 @@ const AddParking = () => {
               </select>
             </div> */}
             <div className="grid  items-center ">
-                <label htmlFor="" className="font-semibold">Parking Number</label>
-                <select name="parking_id" id="" 
-                 value={formData.parking_id}
-                 onChange={handleChange}
-                className="border p-1 px-4 border-gray-500 rounded-md">
-                  <option value="">Select Parking Number</option>
-                  {ParkConfigData?.map((floor) => (
+              <label htmlFor="" className="font-semibold">
+                Parking Number
+              </label>
+              <select
+                name="parking_id"
+                id=""
+                value={formData.parking_id}
+                onChange={handleChange}
+                className="border p-1 px-4 border-gray-500 rounded-md"
+              >
+                <option value="">Select Parking Number</option>
+                {ParkConfigData?.map((floor) => (
                   <option value={floor.id} key={floor.id}>
-                    {floor.name} 
+                    {floor.name}
                   </option>
                 ))}
-                </select>
-              </div>
-              </div>
-            {/*
+              </select>
+            </div>
+          </div>
+          {/*
           </div> */}
- 
+
           <div className="flex justify-center my-2">
-            <button 
-            onClick={handleSubmit}
-            style={{ background: themeColor }}
-            className="p-1 px-4 bg-black text-white hover:bg-white  rounded-md border-2 border-black font-medium transition-all duration-300">
+            <button
+              onClick={handleSubmit}
+              style={{ background: themeColor }}
+              className="p-1 px-4 bg-black text-white hover:bg-white  rounded-md border-2 border-black font-medium transition-all duration-300"
+            >
               Submit
             </button>
           </div>
