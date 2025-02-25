abf65713a67a5d5b20b6c20a0201be9571a08e80
vishal89220 <vy9282677@gmail.com>
2025-02-25 11:19:43 +0530

    incident api

--- src/containers/modals/IncidentInjuryModal.jsx
+++ src/containers/modals/IncidentInjuryModal.jsx
@@ -1,20 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ModalWrapper from "./ModalWrapper";
 import { MdClose } from "react-icons/md";
 import { FaCheck, FaTrash } from "react-icons/fa";
 import { PiPlusCircleBold } from "react-icons/pi";
-import {postInjurydata,getIncidentData} from "../../api"
+import { postInjurydata, getInjured } from "../../api";
 import { useParams } from "react-router-dom";
 import toast from "react-hot-toast";
-const IncidentInjuryModal = ({ onclose }) => {
-const { id } = useParams();
-  
+const IncidentInjuryModal = ({ onclose ,onsave }) => {
+  const { id } = useParams();
+
   const [incident, setIncident] = useState([{ name: "", mobile: "" }]);
   const [injury, setInjury] = useState();
   const [injuryType, setInjuryType] = useState("");
   const [whoGotInjured, setWhoGotInjured] = useState("");
-  const [companyName , setCompanyName] = useState("")  
+  const [whoGotInjuredOptions, setWhoGotInjuredOptions] = useState([]);
 
+  const [companyName, setCompanyName] = useState("");
+  console.log(whoGotInjured);
   const handleAddIncident = (event) => {
     event.preventDefault();
     setIncident([...incident, { name: "", mobile: "" }]);
@@ -33,22 +35,21 @@ const { id } = useParams();
     setIncident(newIncident);
   };
 
-
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = {
-       incident_id: id,
+      incident_id: id,
       injuries: incident.map((incident1) => ({
         injury_type: injuryType,
         injury_number: "",
         lost_time: "",
-        who_got_injured_id: whoGotInjured,
+        who_got_injured: whoGotInjured,
         name: incident1.name,
         company_name: incident1.companyName,
         mobile: incident1.mobile,
       })),
     };
-    if (!injuryType || !whoGotInjured) {
+    if (!injuryType) {
       toast.error("Please select injury type and who got injured");
       return;
     }
@@ -60,28 +61,45 @@ const { id } = useParams();
     });
     postInjurydata(data)
       .then((response) => {
-        toast.success('Injury data submitted successfully!');
+        toast.success("Injury data submitted successfully!");
         console.log(response);
-        onclose()
+        onclose();
+        onsave();
         // Handle success response
       })
       .catch((error) => {
         console.error(error);
-        toast.error('Error submitting injury data: ' + error.message);
+        toast.error("Error submitting injury data: " + error.message);
         // Handle error response
         alert("Error: " + error.message);
       });
   };
 
+  const fetchInjuredData = async () => {
+    try {
+      const res = await getInjured("injury");
+      const whoGotInjuredOptions = res.data.map((item) => ({
+        id: item.id,
+        name: item.name,
+      }));
+      setWhoGotInjured(whoGotInjuredOptions[0].name);
+      setWhoGotInjuredOptions(whoGotInjuredOptions);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    fetchInjuredData();
+  }, []);
+
   return (
     <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-30 backdrop-blur-sm z-20">
       <div className="bg-white overflow-auto max-h-[80%]  md:w-auto w-96 p-2 pt-4 px-4 flex flex-col rounded-xl gap-2">
-            <h1 className="font-semibold text-center text-xl border-b flex items-center gap-2 justify-center">
-              <PiPlusCircleBold /> Add Injury
-            </h1>
+        <h1 className="font-semibold text-center text-xl border-b flex items-center gap-2 justify-center">
+          <PiPlusCircleBold /> Add Injury
+        </h1>
         <div className="overflow-y-auto hide-scrollbar">
           <div className="flex flex-col gap-2 z-10">
-
             {incident.map((incident1, index) => (
               <div key={index} className="bg-green-50 rounded-md p-1 border-b">
                 <div className="grid gap-x-5 gap-y-2">
@@ -115,11 +133,21 @@ const { id } = useParams();
                     </label>
                     <select
                       name="whoGotInjured"
-                      id={whoGotInjured}
+                      value={whoGotInjured}
                       onChange={(event) => setWhoGotInjured(event.target.value)}
                       className="border p-2 border-gray-400 rounded-md w-full"
                     >
                       <option value="">Select </option>
+                      {Array.isArray(whoGotInjuredOptions) &&
+                        whoGotInjuredOptions.map((level) => (
+                          <option
+                            value={level.name}
+                            id={level.id}
+                            key={level.id}
+                          >
+                            {level.name}
+                          </option>
+                        ))}
                     </select>
                   </div>
                 </div>
@@ -167,13 +195,13 @@ const { id } = useParams();
                     />
                   </div>
                 </div>
-                
+
                 <div className="flex justify-end border-b">
                   <button
                     className="bg-red-400 p-2 px-4 text-white rounded-md my-2"
                     onClick={() => handleRemoveIncident(index)}
                   >
-                    <FaTrash/>
+                    <FaTrash />
                   </button>
                 </div>
               </div>
@@ -184,17 +212,22 @@ const { id } = useParams();
                 className="bg-green-400 p-2 px-4 text-white rounded-md flex items-center gap-2"
                 onClick={handleAddIncident}
               >
-               <PiPlusCircleBold/> Add More
+                <PiPlusCircleBold /> Add More
               </button>
             </div>
-           
           </div>
         </div>
         <div className="flex justify-center gap-2 border-t p-1">
-          <button className="bg-red-400 text-white p-2 px-4 rounded-full flex items-center gap-2" onClick={()=> onclose()}>
+          <button
+            className="bg-red-400 text-white p-2 px-4 rounded-full flex items-center gap-2"
+            onClick={() => onclose()}
+          >
             <MdClose /> Cancel
           </button>
-          <button className="bg-green-400 text-white p-2 px-4 rounded-full flex items-center gap-2" onClick={handleSubmit}>
+          <button
+            className="bg-green-400 text-white p-2 px-4 rounded-full flex items-center gap-2"
+            onClick={handleSubmit}
+          >
             <FaCheck /> Submit
           </button>
         </div>
