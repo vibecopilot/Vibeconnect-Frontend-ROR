cc4dbda26b0af0a3f5e1efd24251e9f7447fcd21
akshay <akshaymugale1@gnmail.com>
2025-06-28 16:59:56 +0530

    Updated By User and border layout in checklist creation

--- src/pages/SubPages/details/AssetRoutineDetails.jsx
+++ src/pages/SubPages/details/AssetRoutineDetails.jsx
@@ -11,7 +11,7 @@ const AssetRoutineDetails = () => {
     toast.loading("Please wait");
     const fetchTaskDetails = async () => {
       const detailsResp = await getRoutineTaskDetails(assetId, activityId);
-      toast.dismiss()
+      toast.dismiss();
       toast.success("Schedule Details fetched successfully");
       console.log(detailsResp.data);
       setTaskDetails(detailsResp.data);
@@ -19,7 +19,7 @@ const AssetRoutineDetails = () => {
     fetchTaskDetails();
   }, [assetId, activityId]);
 
-  if (taskDetails.length === 0)  {
+  if (taskDetails.length === 0) {
     return (
       <div className="flex  justify-center items-center w-full h-screen p-2">
         <p>No details available</p>
@@ -27,7 +27,7 @@ const AssetRoutineDetails = () => {
     );
   }
 
-  const { asset_name, checklist_name, created_at } = taskDetails[0];
+  const { asset_name, checklist_name, created_at, user_name } = taskDetails[0];
   const dateFormat = (dateString) => {
     const date = new Date(dateString);
     return date.toLocaleDateString("en-GB", {
@@ -52,15 +52,18 @@ const AssetRoutineDetails = () => {
           <p className="font-medium">Checklist Name :</p>
           <p>{checklist_name}</p>
         </div>
+
+        <div className="grid grid-cols-2 gap-1">
+          <p className="font-medium">Updated By : </p>
+          <p>{user_name}</p>
+        </div>
       </div>
       <div className="w-full my-2">
-       
         {taskDetails.map((task, index) => (
           <div key={task.id}>
             {/* {index === 0 ? null : ( */}
-              <>
+            <>
               <div className="my-4 flex flex-col bg-gray-100 shadow-custom-all-sides p-2 rounded-md gap-2">
-
                 <div className="flex gap-4 items-center">
                   <p className="font-medium">Question :</p>
                   <p>{task.question_name}</p>
@@ -68,34 +71,30 @@ const AssetRoutineDetails = () => {
                 <div className="flex gap-4 items-center bg-green-200 p-2 rounded-md">
                   <p className="font-medium">Answer :</p>
                   <p>{task.value}</p>
-              </div>
-              <span className="font-medium text-gray-500">
-                                Attachments :
-                              </span>
-                              <div className="flex gap-4 flex-wrap my-4 items-center text-center">
-                                {task.question_attachments?.length > 0 ? (
-                                  task.question_attachments.map(
-                                    (attachment, i) => (
-                                      <img
-                                        key={i}
-                                        src={domainPrefix + attachment.document}
-                                        alt={`Attachment ${i + 1}`}
-                                        className="w-40 h-28 object-cover rounded-md"
-                                        onClick={() =>
-                                          window.open(
-                                            domainPrefix + attachment.document,
-                                            "_blank"
-                                          )
-                                        }
-                                      />
-                                    )
-                                  )
-                                ) : (
-                                  <p>No Attachments</p>
-                                )}
-                              </div>
                 </div>
-              </>
+                <span className="font-medium text-gray-500">Attachments :</span>
+                <div className="flex gap-4 flex-wrap my-4 items-center text-center">
+                  {task.question_attachments?.length > 0 ? (
+                    task.question_attachments.map((attachment, i) => (
+                      <img
+                        key={i}
+                        src={domainPrefix + attachment.document}
+                        alt={`Attachment ${i + 1}`}
+                        className="w-40 h-28 object-cover rounded-md"
+                        onClick={() =>
+                          window.open(
+                            domainPrefix + attachment.document,
+                            "_blank"
+                          )
+                        }
+                      />
+                    ))
+                  ) : (
+                    <p>No Attachments</p>
+                  )}
+                </div>
+              </div>
+            </>
             {/* )} */}
           </div>
         ))}
