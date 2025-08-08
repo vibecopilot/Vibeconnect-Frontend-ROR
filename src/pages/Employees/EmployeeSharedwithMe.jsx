976a3df63d140635deec0e065c48a8d437a19853
akshay <akshaymugale1@gnmail.com>
2025-08-08 22:23:49 +0530

    domain prefix

--- src/pages/Employees/EmployeeSharedwithMe.jsx
+++ src/pages/Employees/EmployeeSharedwithMe.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 // import { toast } from "react-toastify";
-import { deleteFilePersonal, deleteFolderPersonal, deleteShareFile, deleteShareFolder, getFolderDocumentCommon, getFolderDocumentPersonal, getSetupUsers, getSharedwith, getSubFolderDocumentCommon, postFileDocumentCommon, postFolderDocumentPersonal, postSharePersonal } from "../../api";
+import { deleteFilePersonal, deleteFolderPersonal, deleteShareFile, deleteShareFolder, domainPrefix, getFolderDocumentCommon, getFolderDocumentPersonal, getSetupUsers, getSharedwith, getSubFolderDocumentCommon, postFileDocumentCommon, postFolderDocumentPersonal, postSharePersonal } from "../../api";
 import { getItemInLocalStorage } from "../../utils/localStorage";
 import { FaEllipsisV, FaFile, FaFolder, FaPlus, FaUpload } from "react-icons/fa";
 import { useSelector } from "react-redux";
@@ -404,7 +404,7 @@ const openFolder = async (folder) => {
   >
     {file && file.document_url ? (
                <a
-                 href={`http://13.215.74.38/${file.document_url}`}
+                 href={`${domainPrefix}/${file.document_url}`}
                  target="_blank"
                  rel="noopener noreferrer"
                  className="flex flex-col items-center"
