5279a537c32d8b0a49abf6ae91255f06f9a085e9
akshay <akshaymugale1@gnmail.com>
2025-11-13 12:11:33 +0530

    index.js

---
+++ src/pages/Accounting/index.js
@@ -0,0 +1,16 @@
+// Accounting Main Pages
+export { default as AccountingDashboard } from './AccountingDashboard';
+export { default as Invoices } from './Invoices';
+export { default as Ledgers } from './Ledgers';
+export { default as JournalEntries } from './JournalEntries';
+export { default as Reports } from './Reports';
+export { default as AccountingSettings } from './AccountingSettings';
+
+// Forms
+export { default as CreateInvoice } from './CreateInvoice';
+
+// Modals
+export { default as CreateLedgerModal } from './CreateLedgerModal';
+
+// Components
+export { default as AccountingNavbar } from '../../components/navbars/AccountingNavbar';
