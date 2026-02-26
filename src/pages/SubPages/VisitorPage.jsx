38907a579f4801b99a3e865ca7798eb900868fe3
Vibhu Mathur <mathurvibhu865@gmail.com>
2026-02-26 18:03:31 +0530

    commit

--- src/pages/SubPages/VisitorPage.jsx
+++ src/pages/SubPages/VisitorPage.jsx
@@ -307,25 +307,45 @@ const VisitorPage = () => {
     setCurrentPage(1);
   };
 
-  useEffect(() => {
-    if (page === "Visitor In") {
-      if (selectedVisitor === "expected") {
-        const expectedIn = visitorIn.filter(
-          (visit) => visit.usertype !== "security_guard"
-        );
-        setFilteredData(expectedIn);
-      } else {
-        const unexpectedIn = visitorIn.filter(
-          (visit) => visit.usertype === "security_guard"
-        );
-        setFilteredUnexpectedVisitor(unexpectedIn);
-      }
-    } else if (page === "all") {
-      setFilteredExpectedVisitor(expectedVisitor);
-      setFilteredUnexpectedVisitor(unexpectedVisitor);
+  // useEffect(() => {
+  //   if (page === "Visitor In") {
+  //     if (selectedVisitor === "expected") {
+  //       const expectedIn = visitorIn.filter(
+  //         (visit) => visit.usertype !== "security_guard"
+  //       );
+  //       setFilteredData(expectedIn);
+  //     } else {
+  //       const unexpectedIn = visitorIn.filter(
+  //         (visit) => visit.usertype === "security_guard"
+  //       );
+  //       setFilteredUnexpectedVisitor(unexpectedIn);
+  //     }
+  //   } else if (page === "all") {
+  //     setFilteredExpectedVisitor(expectedVisitor);
+  //     setFilteredUnexpectedVisitor(unexpectedVisitor);
+  //   }
+  // }, [page, selectedVisitor, visitorIn, expectedVisitor, unexpectedVisitor]);
+useEffect(() => {
+  if (page === "Visitor In") {
+    if (selectedVisitor === "expected") {
+      // Filter expected visitors (exclude 'security_guard')
+      const expectedIn = visitorIn.filter(
+        (visit) => visit.user_type !== "security_guard" // Corrected field name
+      );
+      setFilteredData(expectedIn);
+    } else {
+      // Filter unexpected visitors (include only 'security_guard')
+      const unexpectedIn = visitorIn.filter(
+        (visit) => visit.user_type === "security_guard" // Corrected field name
+      );
+      setFilteredUnexpectedVisitor(unexpectedIn);
     }
-  }, [page, selectedVisitor, visitorIn, expectedVisitor, unexpectedVisitor]);
-
+  } else if (page === "all") {
+    // For 'all' page, display both expected and unexpected visitors
+    setFilteredExpectedVisitor(expectedVisitor);
+    setFilteredUnexpectedVisitor(unexpectedVisitor);
+  }
+}, [page, selectedVisitor, visitorIn, expectedVisitor, unexpectedVisitor]);
   useEffect(() => {
     const fetchExpectedVisitor = async () => {
       try {
