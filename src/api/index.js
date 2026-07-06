69547af62e82a756d6474f9599650d0bb8476085
Pratiksha Jadhav <pratikshajadhav0372@gmail.com>
2026-07-06 17:02:59 +0530

    amenities changes

--- src/api/index.js
+++ src/api/index.js
@@ -11567,6 +11567,7 @@ export const getOtherProject = (siteId) =>
     },
     params: siteId ? { site_id: siteId } : {},
   });
+  
 
 export const postOtherProject = (data) =>
   axiosInstance.post("/other_projects.json", data);
